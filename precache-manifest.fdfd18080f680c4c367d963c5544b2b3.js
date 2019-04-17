da358ab0041d17b176a92127ac67524890c60334
Vita Mileva <milayevavita27@gmail.com>
2019-04-18 01:39:16 +0300

    Updates

---
+++ precache-manifest.fdfd18080f680c4c367d963c5544b2b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a47d9a300047b122cfe1",
+    "url": "/road_test/static/css/main.e3bb8560.chunk.css"
+  },
+  {
+    "revision": "a47d9a300047b122cfe1",
+    "url": "/road_test/static/js/main.1ce18217.chunk.js"
+  },
+  {
+    "revision": "b415e720fcdbe930defe",
+    "url": "/road_test/static/js/runtime~main.a22ed363.js"
+  },
+  {
+    "revision": "9bfbaf68e9023cb576e6",
+    "url": "/road_test/static/js/2.55d128ae.chunk.js"
+  },
+  {
+    "revision": "6048fab488f208e6305d18c5eb30938a",
+    "url": "/road_test/static/media/avatar.6048fab4.png"
+  },
+  {
+    "revision": "a6cc81075cc1c4e6d3901c9bf9abd3a6",
+    "url": "/road_test/static/media/222.a6cc8107.png"
+  },
+  {
+    "revision": "83b3a22585e79b3a0f7f1a4afd63aaeb",
+    "url": "/road_test/static/media/2_1.83b3a225.png"
+  },
+  {
+    "revision": "84c97f59c60859dea1677357e33fb7ed",
+    "url": "/road_test/static/media/avatar3.84c97f59.png"
+  },
+  {
+    "revision": "258fdd49d693f50c66690b3318dbab74",
+    "url": "/road_test/index.html"
+  }
+];
